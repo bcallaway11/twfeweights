404b576a963066668549590ce1232addd403d663
Brantly Callaway <brantly.callaway@uga.edu>
2024-07-08 17:58:00 -0400

    fix description file

--- R/imports.R
+++ R/imports.R
@@ -1,18 +1,18 @@
 #' @title TWFE Weights
-#' 
+#'
 #' @description A package to compute implicit weights from using two-way
-#'  fixed effects (TWFE) regressions in the context of causal inference 
-#'  with panel data.  Besides implicit TWFE regression weights, the package 
+#'  fixed effects (TWFE) regressions in the context of causal inference
+#'  with panel data.  Besides implicit TWFE regression weights, the package
 #'  also includes weights for other causal inference strategies.
-#' 
+#'
 #' @name twfeweights
-#' 
+#'
 #' @import BMisc
 #' @import fixest
 #' @importFrom dplyr arrange arrange_all
 #' @import DRDID
 #' @import ggplot2
-#' @import stats 
+#' @import stats
 #' @import rlang
 #' @importFrom utils tail
 #' @importFrom magrittr %>%
