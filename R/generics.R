708eaeb12ff4083a7f1bc7367c6d7862ddecb91b
Brantly Callaway <brantly.callaway@uga.edu>
2024-10-27 16:34:30 -0400

    export ggtwfeweights

--- R/generics.R
+++ R/generics.R
@@ -1,3 +1,10 @@
+#' @title ggtwfeweights
+#' @description a generic function to plot balance statistics
+#' @param x a `decomposed_twfe` object that comes from a
+#'  previous step of running either `twfe_cov_bal` or `aipw_cov_bal`.
+#' @param ... extra arguments, not used here
+#' @return a ggplot object
+#' @export
 ggtwfeweights <- function(x, ...) {
   UseMethod("ggtwfeweights")
 }
